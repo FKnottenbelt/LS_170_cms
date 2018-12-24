6037546f00cf3bc7f99ff3cc8dddc7d41ddecec6
Femke Knottenbelt <femknotgithub@gmail.com>
2018-12-24 12:29:35 +0000

    Add requirement 4

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -1,21 +1,24 @@
-ENV["RACK_ENV"] = "test"
+require_relative './racktest_helper'
 
-require "minitest/autorun"
-require "rack/test"
+class CmsTest < RackTestCase
 
-require_relative "../cms"
-
-class AppTest < Minitest::Test
-  include Rack::Test::Methods
-
-  def app
-    Sinatra::Application
+  def test_rake_test_have_run
+    puts "Rake tests running"
   end
 
-  def test_index
+  def test_home_page
     get "/"
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_equal "Hello, world!", last_response.body
+    assert_includes(last_response.body, "about.txt")
+    assert_includes(last_response.body, "changes.txt")
+    assert_includes(last_response.body, "history.txt")
+  end
+
+  def test_file_page
+    get "/about.txt"
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes(last_response.body, "Ruby was influenced by Perl")
   end
 end
